b1e8990b9ea8831f2cc8a11e5e4e96dbb279cd8b
devzizu <jazevedo960@gmail.com>
2022-04-18 14:03:18 +0100

    Add script to display a summary table of results

---
+++ tests/filebench-workloads/summary.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#--------------------------------------------------------
+# Edit this variables
+
+RESULTS_FOLDER="."
+
+#--------------------------------------------------------
+
+printf '~%.0s' {1..71}
+printf "\n| %12s | %12s | %10s | %9s | %12s |\n" "workload" "filesystem" "page size" "ops (#/s)" "rd/wr (mb/s)"
+printf '~%.0s' {1..71}
+
+results=""
+
+for result_file in $(find $RESULTS_FOLDER/ -type f -print)
+do
+    summaries=`grep 'Summary' $result_file`
+    AVG_OPS=0
+    AVG_RDWR=0
+    while read -r line
+    do  
+        ops_raw=`grep -o "ops.*ops/s" <<< $line`
+        rd_wr_raw=`grep -o "rd/wr.*mb/s" <<< $line`
+        ops_val=`grep -Eo '[+-]?[0-9]+([.][0-9]+)?' <<< $ops_raw`
+        rd_wr_val=`grep -Eo '[+-]?[0-9]+([.][0-9]+)?' <<< $rd_wr_raw`
+        AVG_OPS=`awk "BEGIN{ print $AVG_OPS + $ops_val }"`
+        AVG_RDWR=`awk "BEGIN{ print $AVG_RDWR + $rd_wr_val }"`
+    done <<< "$summaries"
+    
+    AVG_OPS=`awk "BEGIN{ printf \"%.2f\", ($AVG_RDWR / 3.0) }"`
+    AVG_RDWR=`awk "BEGIN{ printf \"%.2f\", ($AVG_RDWR / 3.0) }"`
+    
+    workload_name=`grep -o "/.*/" <<< $result_file | tr -d '/'`
+    app_name=`grep -o "lazyfs-" <<< $result_file | tr -d '-'`
+    if [ -z $app_name ];
+    then
+        app_name="passthrough"
+    fi
+    page_size=`grep -o "page\-.*k" <<< $result_file | tr -d 'k' | tr -d '-'`
+    if [ -z $page_size ];
+    then
+        page_size="-"
+    else
+        page_size=${page_size#"page"}
+    fi
+    curr=`printf "| %12s | %12s | %10s | %9s | %12s |" $workload_name $app_name $page_size $AVG_OPS $AVG_RDWR`
+    results="$results$curr\n"
+done
+
+echo -e  "$results" | sort
+printf '~%.0s' {1..71} && printf '\n'
