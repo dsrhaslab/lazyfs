1e31d68f604b7f38c490f9e49eba84c2f5f96a78
devzizu <jazevedo960@gmail.com>
2022-04-21 09:18:15 +0100

    Add debugging output for PosgreSQL VACUUM ANALYZE operation

--- tests/util/run-passthrough.sh
+++ tests/util/run-passthrough.sh
@@ -4,4 +4,4 @@ MOUNT_DIR=/tmp/passthrough
 ROOT_DIR=/tmp/passthrough-root
 rm -rf $MOUNT_DIR $ROOT_DIR
 mkdir -p $MOUNT_DIR $ROOT_DIR
-$PASSTBIN $MOUNT_DIR -o allow_other -o modules=subdir -o subdir=$ROOT_DIR -f
+$PASSTBIN $MOUNT_DIR -o allow_other -o modules=subdir -o subdir=$ROOT_DIR -f 2>&1 | tee output
