95476b0be70e5a8c4629b5e7964e8535975d3bfa
devzizu <jazevedo960@gmail.com>
2022-04-25 13:59:13 +0100

    Add docs for FUSE passthrough run script

--- tests/util/fuse/run-passthrough.sh
+++ tests/util/fuse/run-passthrough.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+#---------------------------------------------------------
+# Just a simple script to run pre-compiled binary of the
+# FUSE passthrough filesystem. 
+#
+# Edit this variables:
+
 PASSTBIN=/home/gsd/testbed/libfuse/example/passthrough
 MOUNT_DIR=/mnt/test-fs/passthrough
 ROOT_DIR=/mnt/test-fs/passthrough-root
+
+#---------------------------------------------------------
+
 rm -rf $MOUNT_DIR $ROOT_DIR
 mkdir -p $MOUNT_DIR $ROOT_DIR
 $PASSTBIN $MOUNT_DIR -o allow_other -o modules=subdir -o subdir=$ROOT_DIR -f
