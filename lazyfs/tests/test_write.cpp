e1d1a26cf19790bae03534070a049038c9a8040a
devzizu <jazevedo960@gmail.com>
2022-06-04 16:42:40 +0100

    LazyFS Tests: Add more write tests to test_write.cpp

--- lazyfs/tests/test_write.cpp
+++ lazyfs/tests/test_write.cpp
@@ -41,7 +41,7 @@ TEST (WriteTests, WriteSeq1) {
     ASSERT_EQ (pwrite (fd, buf, IO_BLOCK_SIZE, IO_BLOCK_SIZE), IO_BLOCK_SIZE);
     ASSERT_EQ (lseek (fd, 0, SEEK_END), 2 * IO_BLOCK_SIZE);
 
-    close (fd);
+    ASSERT_TRUE (close (fd) >= 0);
 }
 
 TEST (WriteTests, WriteSeq1CheckIntegrity) {
@@ -59,7 +59,7 @@ TEST (WriteTests, WriteSeq1CheckIntegrity) {
     ASSERT_EQ (pread (fd, buf, IO_BLOCK_SIZE, IO_BLOCK_SIZE), IO_BLOCK_SIZE);
     ASSERT_TRUE (!memcmp (buf, buf_expected, IO_BLOCK_SIZE));
 
-    close (fd);
+    ASSERT_TRUE (close (fd) >= 0);
 
     ASSERT_TRUE (unlink ("WriteSeq1") >= 0);
 }
@@ -88,7 +88,7 @@ TEST (WriteTests, WriteBlockEdges1) {
     ASSERT_EQ (pwrite (fd, buf, IO_BLOCK_SIZE, 2 * IO_BLOCK_SIZE), IO_BLOCK_SIZE);
     ASSERT_EQ (lseek (fd, 0, SEEK_END), 3 * IO_BLOCK_SIZE);
 
-    close (fd);
+    ASSERT_TRUE (close (fd) >= 0);
 }
 
 TEST (WriteTests, WriteBlockEdges1CheckIntegrity) {
@@ -109,9 +109,53 @@ TEST (WriteTests, WriteBlockEdges1CheckIntegrity) {
     ASSERT_EQ (buf[0], 'E');
     ASSERT_EQ (buf[1], 0);
 
-    close (fd);
+    ASSERT_TRUE (close (fd) >= 0);
 
     ASSERT_TRUE (unlink ("WriteSeq1WriteBlockEdges1") >= 0);
+}
+
+TEST (WriteTests, WriteSparseBlocks1) {
+
+    int fd = open ("WriteSparseBlocks1", O_CREAT | O_WRONLY, 0640);
+
+    ASSERT_TRUE (fd >= 0);
+
+    char buf[IO_BLOCK_SIZE];
+    memset (buf, 'A', 10);
+
+    // Write 10 bytes at the end of block 0
+    ASSERT_EQ (pwrite (fd, buf, 10, IO_BLOCK_SIZE - 10), 10);
+    ASSERT_EQ (lseek (fd, 0, SEEK_END), IO_BLOCK_SIZE);
+
+    // Write 20 bytes at block 1 to 2 (-10 -> +10)
+    memset (buf, 'B', 20);
+    ASSERT_EQ (pwrite (fd, buf, 20, 2 * IO_BLOCK_SIZE - 10), 20);
+    ASSERT_EQ (lseek (fd, 0, SEEK_END), 2 * IO_BLOCK_SIZE + 10);
+
+    // Rewrite 20 bytes from block
+
+    ASSERT_TRUE (close (fd) >= 0);
+}
+
+TEST (WriteTests, WriteSparseBlocks1CheckIntegrity) {
+
+    int fd = open ("WriteSparseBlocks1", O_RDONLY, 0640);
+
+    ASSERT_TRUE (fd >= 0);
+
+    char buf[IO_BLOCK_SIZE * 2 + 10];
+    char buf_expected[IO_BLOCK_SIZE * 2 + 10];
+    memset (buf_expected, 0, IO_BLOCK_SIZE - 10);
+    memset (buf_expected + IO_BLOCK_SIZE - 10, 'A', 10);
+    memset (buf_expected + IO_BLOCK_SIZE, 0, IO_BLOCK_SIZE - 10);
+    memset (buf_expected + IO_BLOCK_SIZE * 2 - 10, 'B', 20);
+
+    ASSERT_EQ (pread (fd, buf, IO_BLOCK_SIZE * 2 + 10, 0), IO_BLOCK_SIZE * 2 + 10);
+    ASSERT_TRUE (!memcmp (buf, buf_expected, IO_BLOCK_SIZE * 2 + 10));
+
+    ASSERT_TRUE (close (fd) >= 0);
+
+    ASSERT_TRUE (unlink ("WriteSparseBlocks1") >= 0);
 }
 
 } // namespace lazyfs::tests
